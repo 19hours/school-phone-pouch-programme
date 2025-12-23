66cfc6ac05576a9d85929b3b2c08af0d6c8540a2
Kevin Chu <kevinchuwinghim@gmail.com>
2025-12-23 10:32:26 +0800

    Temporarily remove gtag

--- components/TrackedLink.tsx
+++ components/TrackedLink.tsx
@@ -1,23 +1,23 @@
-// components/TrackedLink.tsx
-"use client";
+// // components/TrackedLink.tsx
+// "use client";
 
-import Link from "next/link";
-import { gtagEvent } from "@/lib/gtag";
-import type { ComponentProps } from "react";
+// import Link from "next/link";
+// import { gtagEvent } from "@/lib/gtag";
+// import type { ComponentProps } from "react";
 
-type Props = ComponentProps<typeof Link> & {
-  eventName: string;
-  eventParams?: Record<string, any>;
-};
+// type Props = ComponentProps<typeof Link> & {
+//   eventName: string;
+//   eventParams?: Record<string, any>;
+// };
 
-export function TrackedLink({ eventName, eventParams, ...props }: Props) {
-  return (
-    <Link
-      {...props}
-      onClick={(e) => {
-        props.onClick?.(e);
-        gtagEvent(eventName, eventParams);
-      }}
-    />
-  );
-}
+// export function TrackedLink({ eventName, eventParams, ...props }: Props) {
+//   return (
+//     <Link
+//       {...props}
+//       onClick={(e) => {
+//         props.onClick?.(e);
+//         gtagEvent(eventName, eventParams);
+//       }}
+//     />
+//   );
+// }
